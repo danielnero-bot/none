26f1810479517cc404b03a83863c6308a1c492ff
DanielNero <nero80311@gmail.com>
2025-05-21 18:52:50 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.add("show");
+}
+function closeMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.remove("show");
+}
